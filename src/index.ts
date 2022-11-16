dc946aacbcd8e8024c3f366c6e6e040558d71e04
Vincent <vincent@rockx.com>
2022-11-16 17:57:21 +0800

    feat: ts

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,9 @@ export default function matchSwitch<
   R =
     | ReturnType<Extract<T[keyof T], (...args: any[]) => any>>
     | Exclude<T[keyof T], (...args: any[]) => any>
->(value: K, branches: T): R {
-  const target = value in branches ? branches[value] : branches[_ as K];
+>(value: K | null | undefined, branches: T): R {
+  const target =
+    value != null && value in branches ? branches[value] : branches[_ as K];
   if (typeof target == 'function') {
     // @ts-ignore
     return target(value => matchSwitch(value, branches));
