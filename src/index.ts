c852c4eb5be3d888ad104bb7af1117c70f51fb2e
Vincent <vincent@rockx.com>
2022-11-30 11:53:19 +0800

    feat: 回归最简单的模式

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,17 @@
-import { omit } from 'lodash';
-
 export default function matchSwitch<
   K extends PropertyKey,
-  T extends Record<
-    PropertyKey,
-    | Object
-    | null
-    | undefined
-    | (() => Object)
-    | ((value: PropertyKey, next: (value: PropertyKey) => never) => void)
-  >,
+  T extends Record<PropertyKey, any>,
   R =
-    | ReturnType<Extract<T[keyof T], (...args: any[]) => any>>
     | Exclude<T[keyof T], (...args: any[]) => any>
+    | ReturnType<Extract<T[keyof T], (...args: any[]) => any>>
 >(value: K | null | undefined, branches: T): R {
-  const target =
-    $ in branches
-      ? branches[$ as K]
-      : value != null && value in branches
-      ? branches[value]
-      : branches[_ as K];
-  if (typeof target == 'function') {
-    // @ts-ignore
-    return target(value, value => matchSwitch(value, omit(branches, [$])));
+  if (value != null && value in branches) {
+    return branches[value];
   } else {
-    // @ts-ignore
-    return target;
+    return branches[_];
   }
 }
 /**
  * defaultPropertyKey
  */
 export const _ = Symbol();
-/**
- * allPropertyKey
- */
-export const $ = Symbol();
--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,19 +1,12 @@
-import matchSwitch, { $, _ } from '../src';
+import matchSwitch, { _ } from '../src';
 
 describe('blah', () => {
   it('works', () => {
     expect(matchSwitch(1, { 1: 'hello', [_]: 5 })).toEqual('hello');
-    expect(matchSwitch(1, { 1: () => 'hello', [_]: 5 })).toEqual('hello');
-    expect(
-      matchSwitch(1, { 1: (_, next) => next(2), 2: 'hello', [_]: 5 })
-    ).toEqual('hello');
+    expect(matchSwitch(1, { 1: 2, 2: 'hello', [_]: 5 })).toEqual(2);
     expect(matchSwitch(1, { 12: 'hello', [_]: 5 })).toEqual(5);
-    expect(matchSwitch(1, { 12: 'hello', [_]: () => 5 })).toEqual(5);
+    expect(matchSwitch(1, { 12: 'hello', [_]: 5 })).toEqual(5);
     expect(matchSwitch(1, { 12: 'hello', [_]: 5 })).toEqual(5);
     expect(matchSwitch(1, { 12: 'hello', _: 5, [_]: 6 })).toEqual(6);
-    expect(matchSwitch(1, { [$]: '$', [_]: '_', 1: '1' })).toEqual('$');
-    expect(
-      matchSwitch(1, { [$]: (_, next) => next(2), [_]: '_', 1: '1', 2: '2' })
-    ).toEqual('2');
   });
 });
